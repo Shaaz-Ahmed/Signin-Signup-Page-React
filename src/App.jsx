f436d0699486753593e5284bbec28b183472151a
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-08-20 16:22:40 +0530

    added image and imported image into component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import LoginSignup from './Components/LoginSignup/LoginSignup';
 
+import user_icon from '../Assets/person.png';
+import email_icon from '../Assets/email.png';
+import password_icon from '../Assets/password.png';
+
 const App = () =>{
     return(
         <>
