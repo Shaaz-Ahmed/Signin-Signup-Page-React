d7f62bf9007916f17f7eee7b46f0fe87591a27ec
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-08-20 16:39:59 +0530

    added html completely

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import LoginSignup from './Components/LoginSignup/LoginSignup';
 
-import user_icon from '../Assets/person.png';
-import email_icon from '../Assets/email.png';
-import password_icon from '../Assets/password.png';
-
-const App = () =>{
+const App = () => {
     return(
         <>
             <LoginSignup/>
         </>
-    )
+    );
 }
+
 export default App;
